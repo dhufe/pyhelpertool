380ae47550b901ac2c4da63b09859d69a45d3dd6
Daniel Hufschläger <daniel@hufschlager.net>
2020-03-25 07:25:51 +0100

    fixed syntax errors and adding functions for lowpass filtering.

--- pyhelpertool/HelpersFiltering.py
+++ pyhelpertool/HelpersFiltering.py
@@ -6,10 +6,10 @@ def butter_lowpass(cutoff, fs, order=5):
     b, a = sp.butter(order, normal_cutoff, btype='low')
     return b, a
 
-def butter_lowpass(cutoff, nyq_freq, order=4):
-    normal_cutoff = float(cutoff) / nyq_freq
-    b, a = signal.butter(order, normal_cutoff, btype='lowpass',analog=False)
-    return b, a
+def butter_lowpass_filter (data, cutoff, fs, order=4):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = sp.filtfilt(b, a, data)
+    return y
 
 def butter_highpass(cutoff, fs, order=5):
     nyq = 0.5 * fs
@@ -19,7 +19,7 @@ def butter_highpass(cutoff, fs, order=5):
 
 def butter_highpass_filter(data, cutoff, fs, order=5):
     b, a = butter_highpass(cutoff, fs, order=order)
-    y = signal.filtfilt(b, a, data)
+    y = sp.filtfilt(b, a, data)
     return y
 
 def butter_bandpass( lowcut, highcut, fs, order=5 ):
